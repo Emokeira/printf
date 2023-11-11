cafb4db21ba80b0eb6168d4c360b751ae4f3a1c8
Edith Mong'are <edithmokeira7@gmail.com>
2023-11-11 13:02:16 +0300

    Added: _myPutchar.c

---
+++ _myPutchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _myPutchar - writes a character to the standard output
+ * @c: The characters to print
+ *
+ * Return: On succes, 1 is returned, -1 is returned/
+ */
+
+int _myPutchar(char c)
+{
+	return (1, &c, 1);
+}
+
--- main.h
+++ main.h
@@ -2,12 +2,18 @@
 #define MAIN_H
 
 #include <stdarg.h>
+/**
+ * struct print - Struct for handling format specifiers
+ * @t: The format specifier
+ * @f: Pointer to the corresponding prrinting function
+ */
 
 typedef struct print
 {
 	char *t;
-	int(*f)(va_list);
-}print_t;
+	int (*f)(va_list);
+
+} print_t;
 
 #define NUM_HANDLERS 2
 
@@ -17,6 +23,6 @@ extern print_t handlers[NUM_HANDLERS];
 
 int _printf(const char *format, ...);
 int print_s(va_list output);
-int print_c(va_list ouput);
+int print_c(va_list output);
 
 #endif
