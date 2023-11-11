688da226dc08935f6472b2f4a9d544fbd5a4b46c
Edith Mong'are <edithmokeira7@gmail.com>
2023-11-11 14:15:53 +0300

    Added: _printf.c file

--- _printf.c
+++ _printf.c
@@ -1,3 +1,48 @@
 #include "main.h"
 
-int 
+/**
+ * _printf - Custom printf function
+ * @format:Format string
+ *
+ * Return: Number of characters printed(excluding null byte)
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list output;
+	size_t i, num_handlers = NUM_HANDLERS;
+
+	va_start(output, format);
+
+	while (*format)
+	{
+		if (*format == '%' && *(format + 1) != '\0')
+		{
+			if (format[1] == '%')
+			{
+				_myPutchar('%');
+				format += 2;
+			}
+			else
+			{
+				for (i = 0; i < num_handlers; i++)
+				{
+					if (format[1] == handlers[i].t[0])
+					{
+						handlers[i].f(output);
+						break;
+					}
+				}
+				format += 2;
+			}
+		}
+		else
+		{
+			_myPutchar(*format);
+			format++;
+		}
+	}
+
+	va_end(output);
+	return (0);
+}
