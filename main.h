a17e74cc71d8da0b1bc8e49982877b47ccba22a5
Edith Mong'are <edithmokeira7@gmail.com>
2023-11-11 12:22:16 +0300

    Added: Modified main.h

--- main.h
+++ main.h
@@ -1,4 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
+
+typedef struct print
+{
+	char *t;
+	int(*f)(va_list);
+}print_t;
+
+#define NUM_HANDLERS 2
+
+extern print_t handlers[NUM_HANDLERS];
+
+int _print(const char *format, ...0;
+int print_s(va_list output);
+int print_c(va_list ouput);
 
 #endif
