aea77fe5ca0029a158ffb48af1a552a19c4eab3a
Fromb23 <rombo.programming@gmail.com>
2023-11-14 21:40:56 -0800

    separated int d and i

--- Print_decimal.c
+++ Print_decimal.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * print_d_i - Prints a decimal (int) number
+ * print_d - Prints a decimal (int) number
  * @args: Handles arguments containing va_list and buffer pointer
  *
  * Return: The number of characters printed excluding null bytes
  */
 
-int print_d_i(HandlerArgs args)
+int print_d(HandlerArgs args)
 {
-	int custom_d_i = va_arg(args.output, int);
-	int temp = custom_d_i;
+	int custom_d = va_arg(args.output, int);
+	int temp = custom_d;
 	int int_len = (temp < 0) ? 1 : 0;
 	int converted_len;
 
@@ -23,7 +23,7 @@ int print_d_i(HandlerArgs args)
 		return (-1);
 
 	converted_len = snprintf(args.buffer_ptr, BUFFER_SIZE -
-			(args.buffer_ptr - args.buffer_start), "%d",  custom_d_i);
+			(args.buffer_ptr - args.buffer_start), "%d",  custom_d);
 	if (converted_len < 0)
 		return (-1);
 
@@ -31,3 +31,36 @@ int print_d_i(HandlerArgs args)
 
 	return (converted_len);
 }
+/**
+ * print_i - Prints an integer number
+ * @args: Handles arguments containing Va_list and buffer pointer
+ *
+ * Retrurn: The number of characters printed excluding null bytes
+ **/
+
+int print_i(HandlerArgs args)
+
+{
+	int custom_i = va_arg(args.output, int);
+	int temp = custom_i;
+	int int_len = (temp < 0) ? 1 : 0;
+	int converted_len;
+
+	do {
+		temp /= 10;
+		int_len++;
+	} while (temp != 0);
+
+	if (int_len >= BUFFER_SIZE - (args.buffer_ptr - args.buffer_start))
+		return (-1);
+
+	converted_len = snprintf(args.buffer_ptr, BUFFER_SIZE -
+		(args.buffer_ptr - args.buffer_start), "%d", custom_i);
+	if (converted_len < 0)
+		return (-1);
+
+	args.buffer_ptr += converted_len;
+
+	return (converted_len);
+}
+
